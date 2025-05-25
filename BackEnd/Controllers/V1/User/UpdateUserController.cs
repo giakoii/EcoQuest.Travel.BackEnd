97459f9d8c2c4d55dfa5cf2e96a3f95adff5c129
giakoii <khoipgse172073@fpt.edu.vn>
2025-05-25 15:49:51 +0700

    [Add][User] Init Api UpdateUser

--- BackEnd/Controllers/V1/User/UpdateUserController.cs
+++ BackEnd/Controllers/V1/User/UpdateUserController.cs
@@ -1,64 +1,65 @@
-// using Client.SystemClient;
-// using BackEnd.Controllers;
-// using Microsoft.AspNetCore.Authorization;
-// using Microsoft.AspNetCore.Mvc;
-// using NLog;
-// using ProfileService.Services;
-// using ProfileService.Utils.Const;
-//
-// namespace ProfileService.Controllers.V1.Profile;
-//
-// public class Ps010UpdateProfileController : AbstractApiAsyncController<UpdateUserRequest, UpdateUserResponse, string>
-// {
-//     private readonly IProfileService _profileService;
-//     private readonly Logger _logger = LogManager.GetCurrentClassLogger();
-//
-//     /// <summary>
-//     /// Constructor
-//     /// </summary>
-//     /// <param name="profileService"></param>
-//     /// <param name="identityApiClient"></param>
-//     public Ps010UpdateProfileController(IProfileService profileService, IIdentityApiClient identityApiClient)
-//     {
-//         _profileService = profileService;
-//         _identityApiClient = identityApiClient;
-//     }
-//
-//     /// <summary>
-//     /// Incoming Put
-//     /// </summary>
-//     /// <param name="request"></param>
-//     /// <returns></returns>
-//     [HttpPut]
-//     [Authorize(AuthenticationSchemes = OpenIddict.Validation.AspNetCore.OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme)]
-//     public override async Task<UpdateUserResponse> ProcessRequest(UpdateUserRequest request)
-//     {
-//         return await ProcessRequest(request, _logger, new UpdateUserResponse());
-//     }
-//
-//     protected override async Task<UpdateUserResponse> Exec(UpdateUserRequest request)
-//     {
-//         return await _profileService.UpdateProfile(request, _identityEntity);
-//     }
-//
-//     /// <summary>
-//     /// Error Check
-//     /// </summary>
-//     /// <param name="request"></param>
-//     /// <param name="detailErrorList"></param>
-//     /// <returns></returns>
-//     protected internal override UpdateUserResponse ErrorCheck(UpdateUserRequest request, List<DetailError> detailErrorList)
-//     {
-//         var response = new UpdateUserResponse() { Success = false };
-//         if (detailErrorList.Count > 0)
-//         {
-//             // Error
-//             response.SetMessage(MessageId.E10000);
-//             response.DetailErrorList = detailErrorList;
-//             return response;
-//         }
-//         // True
-//         response.Success = true;
-//         return response;
-//     }
-// }
+using BackEnd.Services;
+using BackEnd.SystemClient;
+using BackEnd.Utils.Const;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+using OpenIddict.Validation.AspNetCore;
+
+namespace BackEnd.Controllers.V1.User;
+
+[Route("api/v1/user/[controller]")]
+public class UpdateProfileController : AbstractApiAsyncController<UpdateUserRequest, UpdateUserResponse, string>
+{
+    private readonly IUserService _userService;
+    private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="userService"></param>
+    /// <param name="identityApiClient"></param>
+    public UpdateProfileController(IUserService userService, IIdentityApiClient identityApiClient)
+    {
+        _userService = userService;
+        _identityApiClient = identityApiClient;
+    }
+
+    /// <summary>
+    /// Incoming Put
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    [HttpPut]
+    [Authorize(AuthenticationSchemes = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme)]
+    public override async Task<UpdateUserResponse> ProcessRequest(UpdateUserRequest request)
+    {
+        return await ProcessRequest(request, _logger, new UpdateUserResponse());
+    }
+
+    protected override async Task<UpdateUserResponse> Exec(UpdateUserRequest request)
+    {
+        return await _userService.UpdateUser(request, _identityEntity);
+    }
+
+    /// <summary>
+    /// Error Check
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="detailErrorList"></param>
+    /// <returns></returns>
+    protected internal override UpdateUserResponse ErrorCheck(UpdateUserRequest request, List<DetailError> detailErrorList)
+    {
+        var response = new UpdateUserResponse() { Success = false };
+        if (detailErrorList.Count > 0)
+        {
+            // Error
+            response.SetMessage(MessageId.E10000);
+            response.DetailErrorList = detailErrorList;
+            return response;
+        }
+        // True
+        response.Success = true;
+        return response;
+    }
+}
